45ef096d746b884ccfbdfc59cad88466ca0cfc97
Norbert Versluis <norbert.versluis@live.com>
2020-11-02 12:54:38 +0100

    End of Section 15

--- API/Entitities/AppUser.cs
+++ API/Entitities/AppUser.cs
@@ -25,5 +25,7 @@ namespace API.Entitities
         public ICollection<Photo> Photos { get; set; }
         public ICollection<UserLike> LikedByUsers { get; set; }
         public ICollection<UserLike> LikedUsers { get; set; }
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
     }
 }
